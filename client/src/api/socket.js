9ef894c187218f661513e0996521f846822afb6b
keval-hansaliya <kevalhansaliya@gmail.com>
2026-02-20 00:28:38 +0530

    solved path

--- client/src/api/socket.js
+++ client/src/api/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = import.meta.env.VITE_BACKEND_URL;
+const SOCKET_URL = import.meta.env.VITE_BACKEND_URL.replace(/\/$/, "");
 
 // Create socket instance
 export const socket = io(SOCKET_URL, {
